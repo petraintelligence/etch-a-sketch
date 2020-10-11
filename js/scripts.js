6a3e48d44fdddd864bb9ac1e986958f759b050b0
petraintelligence <brown.joshua.scott@gmail.com>
2020-10-11 13:26:36 -0500

    added event listeners for buttons

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+
+function makeBoard() {
+
+}
+
+function eraseBoard() {
+
+}
+
+function boardSize() {
+
+}
+
+function blackBrush() {
+
+}
+
+function randomBrush() {
+
+}
+
+function changeGradient() {
+
+}
+
+
+
+reset.addEventListener("click", function() { eraseBoard(), makeBoard()});
+changeSize.addEventListener("click", boardSize);
+blackColor.addEventListener("click", blackBrush);
+randomColor.addEventListener("click", randomBrush);
+gradient.addEventListener("click", changeGradient);
+cleared.addEventListener("click", eraseBoard);
